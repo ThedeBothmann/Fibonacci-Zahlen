dfcf7ef983dcdd3186110eafbabdf278de59b529
ThedeBothmann <53815388+ThedeBothmann@users.noreply.github.com>
2019-08-19 11:42:12 +0200

    Add files via upload

---
+++ fibonacci_folge.java
@@ -0,0 +1,13 @@
+public class fibonacci_folge {
+    public static void main (String [] args){
+        int a=0;
+        int b=1;
+        int c;
+        for(int i=1;i<20;i++){
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+    }
+}
