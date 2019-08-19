a603624aa7598d786954281692bffd88819f7bb0
ThedeBothmann <53815388+ThedeBothmann@users.noreply.github.com>
2019-08-19 12:54:54 +0200

    Add files via upload

--- fibonacci_folge.java
+++ fibonacci_folge.java
@@ -3,25 +3,43 @@ import javax.swing.*;
 public class fibonacci_folge {
     public static void main (String [] args){
         int [] fibonaccioffical = {1,1,2,3,5,8,13,21,34,55};
+        boolean weiter=true;
         int a=0;
         int b=1;
         int c=1;
+        int i=1;
         if(c==fibonaccioffical[0]) {
             System.out.println(c);
         }
         else{
             System.out.println("Dieser Code ist falsch");
         }
-        for(int i=1;i<10;i++){
-            c=a+b;
-            a=b;
-            b=c;
-            if(c==fibonaccioffical[i]) {
-                System.out.println(c);
+        while (weiter==true){
+            if(i<10) {
+                c = a + b;
+                a = b;
+                b = c;
+                if (c == fibonaccioffical[i]) {
+                    System.out.println(c);
+                    weiter = true;
+                }
+                else {
+                    System.out.println("Dieser Code ist falsch!");
+                    weiter = false;
+                }
+                i++;
             }
-            else{
-                System.out.println("Dieser Code ist falsch");
+            else {
+                weiter = false;
             }
+        }
+        System.out.println("Dieser Code scheint korrekt zu sein!");
+        int j;
+        for(j=10;j<25;j++){
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(c);
         }
     }
 }
