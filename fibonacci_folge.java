74d9d94f4a07d47ecbbeac2de75b6e8e3cbaa82a
ThedeBothmann <53815388+ThedeBothmann@users.noreply.github.com>
2019-08-21 09:25:54 +0200

    Add files via upload

--- fibonacci_folge.java
+++ fibonacci_folge.java
@@ -1,45 +1,44 @@
 import javax.swing.*;
 
 public class fibonacci_folge {
-    public static void main (String [] args){
-        int [] fibonaccioffical = {1,1,2,3,5,8,13,21,34,55};
-        boolean weiter=true;
-        int a=0;
-        int b=1;
-        int c=1;
-        int i=1;
-        if(c==fibonaccioffical[0]) {
+    public static void main(String[] args) {
+        int[] fibonaccioffical = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+        boolean weiter = true;
+        long a = 0;
+        long b = 1;
+        long c = 1;
+        int i = 1;
+        if (c == fibonaccioffical[0]) {
             System.out.println(c);
-        }
-        else{
+        } else {
             System.out.println("Dieser Code ist falsch");
         }
-        while (weiter==true){
-            if(i<10) {
+        while (weiter == true) {
+            if (i < 10) {
                 c = a + b;
                 a = b;
                 b = c;
                 if (c == fibonaccioffical[i]) {
                     System.out.println(c);
                     weiter = true;
-                }
-                else {
+                } else {
                     System.out.println("Dieser Code ist falsch!");
                     weiter = false;
                 }
                 i++;
-            }
-            else {
+            } else {
+
+                System.out.println("Dieser Code scheint korrekt zu sein!");
+                int j;
+                for (j = 10; j < 92; j++) {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    System.out.println(c);
+                }
                 weiter = false;
             }
         }
-        System.out.println("Dieser Code scheint korrekt zu sein!");
-        int j;
-        for(j=10;j<25;j++){
-            c = a + b;
-            a = b;
-            b = c;
-            System.out.println(c);
-        }
+
     }
 }
